6508834731ad2dabe6c676846d03b45ce1fc9c07
Anuj Singh <anujZzztw@gmail.com>
2022-03-31 01:25:27 +0530

    fixed somethings

--- public/hamburger.js
+++ public/hamburger.js
@@ -1,20 +1,21 @@
 const hamburger = document.querySelector('#btnHamburger');
 const mobile = document.querySelector('.mobileMenu')
 const body = document.querySelector('body');
+const xx = document.querySelector('.mobileMenu');
 
 hamburger.addEventListener('click', function () {
     if (hamburger.classList.contains('open')) {
         mobile.style.display = 'none';
         mobile.style.opacity = '0';
-        body.style.overflow = 'auto';
+
         hamburger.classList.remove('open');
         hamburger.classList.add('close');
     } else {
         mobile.style.opacity = '1';
         mobile.style.display = 'block';
-        body.style.overflow = 'hidden';
+
         hamburger.classList.add('open');
         hamburger.classList.remove('close');
     }
 })
-console.log('click');
+
