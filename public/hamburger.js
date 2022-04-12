9bf343b2edbed2965b7a81b3675d2aa110ba16b9
Anuj Singh <92370865+AnujSsStw@users.noreply.github.com>
2022-04-12 20:53:41 +0530

    feat: add hamburger menu (#52)
    * Added margin
    
    Added margin so that the modcard doesn't overlay the navbar. 
    it works for min 500px screen size.
    
    * hamburger menu done
    
    * fixed somethings
    
    * easy for spotting difference
    
    * Update public/hamburger.js
    
    Co-authored-by: David Leal <halfpacho@gmail.com>
    
    Co-authored-by: David Leal <halfpacho@gmail.com>

---
+++ public/hamburger.js
@@ -0,0 +1,20 @@
+const hamburger = document.querySelector('#btnHamburger');
+const mobile = document.querySelector('.mobileMenu')
+const body = document.querySelector('body');
+const xx = document.querySelector('.mobileMenu');
+
+hamburger.addEventListener('click', function () {
+    if (hamburger.classList.contains('open')) {
+        mobile.style.display = 'none';
+        mobile.style.opacity = '0';
+
+        hamburger.classList.remove('open');
+        hamburger.classList.add('close');
+    } else {
+        mobile.style.opacity = '1';
+        mobile.style.display = 'block';
+
+        hamburger.classList.add('open');
+        hamburger.classList.remove('close');
+    }
+})
